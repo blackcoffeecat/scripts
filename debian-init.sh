afac155a41a9fa850b48e43961428b8b35d73f48
BlackCoffeeCat <allen1995.cngz@gmail.com>
2021-12-09 18:11:01 +0800

    1

--- debian-init.sh
+++ debian-init.sh
@@ -69,7 +69,7 @@ sudo apt-get autoclean
 sudo sed -i '7 a load_module modules/ngx_http_brotli_filter_module.so;' /etc/nginx/nginx.conf
 sudo sed -i '7 a load_module modules/ngx_http_brotli_static_module.so;' /etc/nginx/nginx.conf
 
-sudo curl -o /etc/nginx/conf.d/00nginx.http.conf https://blackcoffeecat.github.io/scripts/nginx.http.conf
+sudo curl -o /etc/nginx/conf.d/00.nginx.http.conf https://blackcoffeecat.github.io/scripts/nginx.http.conf
 
 curl -fsSL https://blackcoffeecat.github.io/scripts/upgrade-nginx.sh | bash -
 
